e470fde4a967fe3ba0f54fe6060d35b151eaf9b1
Austin <austin1@live.unc.edu>
2016-07-12 16:37:59 -0700

    New sketchpads now work

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ $(document).ready(function(){
 	var createPad = function(x){
 		for (var n = 0; n < x; n++){
 	    for (var i = 0; i < x; i++){
-	      $('<div class="boxes"></div>').appendTo('#draw');
+	      $('<div class="boxes"></div>').appendTo('#container');
 	    };
-	    $('<br>').appendTo('#draw');
+	    $('<br>').appendTo('#container');
 		};
 	};
 	createPad(32);
 
-  /*Turns squares black*/
-  $('.boxes').mouseover(function(){
-  	$(this).css('background-color','black');
-  });
 
   /*Clears drawing*/
-  $('button').click(function(){
-  	$('.boxes').css('background-color','white');
-  });
+	$('button').click(function(){
+		$('#container').empty();
+		createPad(prompt('What size would you like?'));
+		$('.boxes').mouseover(function(){
+			$(this).css('background-color','black');
+		});
+	});
+
+
+	/*Turns squares black*/
+	$('.boxes').mouseover(function(){
+		$(this).css('background-color','black');
+	});
 });
