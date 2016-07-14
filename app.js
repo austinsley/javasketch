2f9eb382f77cd77859c96885d04b45f291827ef0
Austin <austin1@live.unc.edu>
2016-07-14 16:20:16 -0700

    Table size limited to 100, separate buttons, better looking page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	/* Draw Grid */
 
 	var createPad = function(x){
 		var boxSize = $('#container').width() / x - 2;
@@ -7,27 +6,44 @@ $(document).ready(function(){
 	    for (var j = 0; j < x; j++){
 	      $('<div class="boxes"></div>').appendTo('#container');
 	    };
-	    $('<br>').appendTo('#container');
+	    $('<div class=lineBrk></div>').appendTo('#container');
 		};
 		$('.boxes').css('height',boxSize);
 		$('.boxes').css('width',boxSize);
 	};
 
+	/* Draws initial grid */
 	createPad(4);
 
+	/*Buttons*/
+	$('#clear').click(function(){
+		$('.boxes').css('background-color','black');
+	});
 
-	/*Clears drawing*/
-	$('button').click(function(){
+	$('#new').click(function(){
 		$('#container').empty();
-		createPad(prompt('What size would you like?'));
+		var newSize = prompt('What size would you like?');
+		var checkNewSize = function(size){
+			if (isNaN(size)){
+				size = prompt("That's not a number, try again.");
+				checkNewSize(size);
+			}
+			else if (size <= 0 || size >= 100){
+				size = prompt("That's not a valid size, try again.");
+				checkNewSize(size);
+			}
+			else {createPad(size)};
+		};
+		checkNewSize(newSize);
+
 		$('.boxes').mouseover(function(){
-			$(this).css('background-color','black');
+			$(this).css('background-color','#006bb3');
 		});
 	});
 
 
-	/*Turns squares black*/
+	/*Colors squares*/
 	$('.boxes').mouseover(function(){
-		$(this).css('background-color','black');
+		$(this).css('background-color','#006bb3');
 	});
 });
