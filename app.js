263c2a2de596af0c09b85a0cb59b6c3a91398da6
Austin <austin1@live.unc.edu>
2016-07-14 22:31:51 -0700

    Added green + blue

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
-$(document).ready(function(){
-
-	var createPad = function(x){
-		var boxSize = $('#container').width() / x - 2;
-		for (var i = 0; i < x; i++){
-	    for (var j = 0; j < x; j++){
-	      $('<div class="boxes"></div>').appendTo('#container');
-	    };
-	    $('<div class=lineBrk></div>').appendTo('#container');
+var createPad = function(x){
+	var boxSize = $('#container').width() / x - 2;
+	for (var i = 0; i < x; i++){
+		for (var j = 0; j < x; j++){
+			$('<div class="boxes"></div>').appendTo('#container');
 		};
-		$('.boxes').css('height',boxSize);
-		$('.boxes').css('width',boxSize);
+		$('<div class=lineBrk></div>').appendTo('#container');
 	};
+	$('.boxes').css('height',boxSize);
+	$('.boxes').css('width',boxSize);
+};
+var color;
+var col;
+var colorChoice;
 
+var updateColor = function(){
+	col = document.getElementsByName('color');
+	for(var i = 0; i < col.length; i++){
+			if(col[i].checked){
+					colorChoice = col[i].value;
+			}
+	};
+	switch(colorChoice) {
+		case 'Red':
+			color = '#f30';
+			break;
+		case 'Green':
+			color = '#3c3';
+			break;
+		case 'Blue':
+			color = '#06f';
+			break;
+	};
+};
+
+$(document).ready(function(){
 	/* Draws initial grid */
+	updateColor();
 	createPad(4);
 
-	/*Buttons*/
+	/*Reset buttons*/
 	$('#clear').click(function(){
 		$('.boxes').css('background-color','black');
 	});
 
 	$('#new').click(function(){
 		$('#container').empty();
-		var newSize = prompt('What size would you like?');
+		var newSize = prompt('What size would you like? 100 > x > 1');
 		var checkNewSize = function(size){
 			if (isNaN(size)){
 				size = prompt("That's not a number, try again.");
@@ -37,13 +60,18 @@ $(document).ready(function(){
 		checkNewSize(newSize);
 
 		$('.boxes').mouseover(function(){
-			$(this).css('background-color','#006bb3');
+			$(this).css('background-color',color);
 		});
 	});
 
-
-	/*Colors squares*/
 	$('.boxes').mouseover(function(){
-		$(this).css('background-color','#006bb3');
+		$(this).css('background-color',color);
 	});
+
+	$('input').click(function(){
+		console.log(colorChoice);
+		updateColor();
+		$('#container').css('border-color',color);
+	});
+
 });
